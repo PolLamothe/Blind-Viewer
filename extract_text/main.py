a845faf179ecd329eea392bd8b450371840b6103
PolLamothe <113200105+PolLamothe@users.noreply.github.com>
2023-03-10 09:51:48 +0100

    start to implement bluetooth

--- extract_text/main.py
+++ extract_text/main.py
@@ -1,7 +1,15 @@
 import pytesseract
+from picamera import PiCamera
 import os
+import time
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Tesseract-OCR\\tesseract.exe'
+
+camera = PiCamera()
+
+camera.resolution = (1920,1080)
+camera.capture('test.webp')
+camera.stop_preview()
 
 if(os.path.exists('test.webp')):
     path = r'test.webp'
