c31b33823d22f01839abdd624decb741c28950ee
BIPBIPGaminG <bipbipgaming.off@gmail.com>
2023-03-12 18:32:36 +0100

    added sentences management(20 chars max and new sentences each \n)

--- extract_text/main.py
+++ extract_text/main.py
@@ -2,6 +2,8 @@ import utils
 from text_manipulation import text
 
 if __name__ == "__main__":
-    formated = utils.format_text(str(text))
-    for word in formated:
-        utils.convert_word(word)
+    txt = r"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit."
+    formated = utils.format_text(txt)
+    for sentence in formated:
+        sent = utils.convert_sentence(sentence)
+        print(sent)
--- extract_text/utils.py
+++ extract_text/utils.py
@@ -2,7 +2,7 @@ import json
 import re
 import os
 
-if(os.path.exists('table.json')): #possible bug avec VSCode qui ne detecte pas table.json avec son path normal
+if os.path.exists('table.json'):  # possible bug avec VSCode qui ne detecte pas table.json avec son path normal
     path = 'table.json'
 else:
     path = './extract_text/table.json'
@@ -11,7 +11,7 @@ with open(path, "r") as table_file:
     table = json.load(table_file)
 
 
-def capital(match_obj): #fonction qui sert a mettre la lettre majuscule en minuscule et a mettre C devant
+def capital(match_obj):  # fonction qui sert a mettre la lettre majuscule en minuscule et a mettre C devant
     if match_obj.group(0) is not None:
         return str("C" + match_obj.group(0).lower())
 
@@ -22,16 +22,47 @@ def number(match_obj):
 
 
 def format_text(text: str) -> list:
+    sentences = []
     words = []
+    length = 0
     for word in text.split(" "):
         word = re.sub(r"[A-Z]", capital, word)
-        #word = re.sub(r"[0-9]", number, word)
-        words.append(word)
-    return words #Retourne une Array avec Chaque mot dans des différentes 'Case' et dans un format compatible avec le table.json
+        word = re.sub(r"[0-9]", number, word)
+        length += len(word) + 1 - ('\n' in word if 2 else 0)
+        if length >= 20:
+            length = 0
+            sentences.append(words.copy())
+            words.clear()
+            if r"\n" in word:
+                length = 0
+                raw_word = re.sub(r"\\n", "", word)
+                words.append(raw_word)
+                sentences.append(words.copy())
+                words.clear()
+            else:
+                words.append(word)
+        elif r"\n" in word:
+            length = 0
+            raw_word = re.sub(r"\\n", "", word)
+            words.append(raw_word)
+            sentences.append(words.copy())
+            words.clear()
+        else:
+            words.append(word)
+    return sentences  # Retourne une Array avec Chaque mot dans des différentes 'Case' et dans un format compatible avec le table.json
 
 
-def convert_word(word:str) -> list: #on convertit le mot en une Array avec dans chaque 'Case' une de ses lettres
+def convert_word(word: str) -> list:  # on convertit le mot en une liste avec dans chaque 'Case' une de ses lettres
     binary = []
     for letter in word:
-        binary.append(table[letter]) #Pour chaque lettre on la convertit en matrice braille et on l'ajoute dans une nouvelle 'case' de binary
-    print(binary)
+        if letter in table:
+            binary.append(table[
+                              letter])  # Pour chaque lettre on la convertit en matrice braille et on l'ajoute dans une nouvelle 'case' de binary
+    return binary
+
+
+def convert_sentence(sentence:list) -> list:
+    new_sentence = []
+    for word in sentence:
+        new_sentence.append(convert_word(word))
+    return new_sentence
